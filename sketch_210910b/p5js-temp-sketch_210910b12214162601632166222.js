3f3f3f691be26aaaa1cde7bc3629afdd54d55fc5
Marzio Cassese <marzio.cassese@edu.tbz.ch>
2021-09-10 11:01:00 +0200

    Initial Commit

---
+++ sketch_210910b/p5js-temp-sketch_210910b12214162601632166222.js
@@ -0,0 +1,16 @@
+function setup() {
+ createCanvas(windowWidth, windowHeight);
+  background(135, 206, 235);
+
+}
+
+function draw(){
+let c = color(255, 204, 0);
+fill(c);
+noStroke();
+ellipse(10,10,190,190);
+
+rect(30, 20, 55, 55);
+
+
+}
