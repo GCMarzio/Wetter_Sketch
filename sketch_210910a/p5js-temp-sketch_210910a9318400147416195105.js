ce810982a111324b305c5b58d0c851baf058d851
Marzio Cassese <marzio.cassese@edu.tbz.ch>
2021-09-10 15:34:41 +0200

    Add sketch a

---
+++ sketch_210910a/p5js-temp-sketch_210910a9318400147416195105.js
@@ -0,0 +1,36 @@
+function setup() {
+createCanvas(windowWidth, windowHeight);
+  background(135, 206, 235);
+}
+
+
+function draw() {
+  
+  let d = color(205,92,92);
+fill(d);
+rect(0,0,1500,200);
+
+
+
+  let c = color(255,255,0);
+fill(c);
+noStroke();
+ellipse(630,180,300,300);
+
+
+let a = color(221,121,7);
+fill(a);
+triangle(1000,700,900,300,300);
+
+
+let b = color(0,0,205);
+fill(b);
+rect(0,200,1500,400);
+
+
+
+
+
+
+}
+  
